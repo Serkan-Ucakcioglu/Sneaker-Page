77915ffd52e99e840eef8d34b9ca0693575d80a2
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-07-23 16:32:37 +0300

    indent

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <script lang="ts" setup>
 //import
-import HeaderBar from './components/HeaderBar.vue';
-import SectionContent from './components/SectionContent.vue';
+import HeaderBar from "./components/HeaderBar.vue";
+import SectionContent from "./components/SectionContent.vue";
 </script>
 <template>
-<!-- Components -->
-  <HeaderBar msg="Welcome to Your Vue.js + TypeScript App"/>
-  <SectionContent/>
+  <!-- Components -->
+  <HeaderBar msg="Welcome to Your Vue.js + TypeScript App" />
+  <SectionContent />
   <!-- Components -->
 </template>
 
-
-
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap");
 #app {
-font-family: 'Kumbh Sans', sans-serif;
+  font-family: "Kumbh Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   font-weight: bold;
 }
-a{
+a {
   text-decoration: none;
 }
 
-
-.container{
+.container {
   width: 90%;
   max-width: 1250px;
   margin: 0px auto;
 }
-
 </style>
